3f8e451e363c03101a8fdb517382e29d92b0bc26
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-01-27 17:48:43 -0300

    feat: Adicionando estilos no navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+
+import { images } from "../../constants";
+
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "sobre mim", "trabalhos", "habilidades", "contatos"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
