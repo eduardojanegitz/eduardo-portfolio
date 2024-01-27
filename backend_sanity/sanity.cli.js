dfe8bbb749e9e9aac91118a33e913d03fc80b868
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-01-27 16:24:14 -0300

    feat: Setup inicial

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '3jgw4pfr',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,17 @@
+import testimonials from './testimonials'
+import about from './about'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  about,
+  contact,
+  experiences,
+  skills,
+  workExperience,
+  works,
+  testimonials,
+]
