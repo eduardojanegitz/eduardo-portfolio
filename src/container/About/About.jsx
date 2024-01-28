2f5f7fe5b10f121353ae30381d920aea4d895f76
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-01-28 16:05:52 -0300

    feat: Finalizado seção about e alterado estrutura de pastas

---
+++ src/container/About/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import { images } from "../../constants";
+import "./About.scss";
+
+const abouts = [
+  {
+    title: "Front-end",
+    description:
+      "Sou familiarizado com a tecnologia javascript, MaterialUI, SASS",
+    imgUrl: images.about01,
+  },
+  {
+    title: "Back-end",
+    description: "Trabalho com NodeJS, PHP",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Metodologias Ágeis",
+    description: "Trabalho com NodeJS, PHP",
+    imgUrl: images.about03,
+  },
+  {
+    title: "Metodologias Ágeis",
+    description: "Trabalho com NodeJS, PHP",
+    imgUrl: images.about04,
+  },
+];
+
+const About = () => {
+  return (
+    <>
+      <h2 className="head-text">
+        Eu sei que
+        <span> Bom desenvolvimento</span>
+        <br />
+        significa
+        <span> Bons negócios</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <h2 className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </h2>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
