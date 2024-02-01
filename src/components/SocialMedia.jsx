78e6851485138551b6190bbd4ace3b74d35a87d8
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-02-01 02:31:58 -0300

    fix: Adicionado links das redes sociais

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -4,15 +4,33 @@ import { BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs";
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      <div>
-        <BsInstagram />
-      </div>
-      <div>
-        <BsLinkedin />
-      </div>
-      <div>
-        <BsGithub />
-      </div>
+      <a
+        href="https://www.instagram.com/eduualvs/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div>
+          <BsInstagram />
+        </div>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/eduardo-alves-091aa1219/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div>
+          <BsLinkedin />
+        </div>
+      </a>
+      <a
+        href="http://github.com/eduardojanegitz"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div>
+          <BsGithub />
+        </div>
+      </a>
     </div>
   );
 };
