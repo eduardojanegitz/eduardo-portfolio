61f68dad00522624144e0de6f65b9c677bd093a8
Eduardo Alves <97857014+eduardojanegitz@users.noreply.github.com>
2024-02-11 12:45:28 -0300

    feat: Finalizado seção de depoimentos, importado novas imagens

--- src/constants/images.js
+++ src/constants/images.js
@@ -34,9 +34,13 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
+import vitafor from "../assets/vitafor.png"
+import facens from "../assets/facens.png"
 
 export default {
   email,
+  vitafor,
+  facens,
   mobile,
   api,
   cpp,
--- src/container/Testimonials/Testimonials.jsx
+++ src/container/Testimonials/Testimonials.jsx
@@ -1,8 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+
 import "./Testimonials.scss";
 
+const brands = [
+  {
+    imgUrl: images.facens,
+    name: "facens",
+  },
+  {
+    imgUrl: images.vitafor,
+    name: "vitafor",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Michelle",
+    company: "Vet",
+    imgUrl: images.redux,
+    feedback: "Eduardo é um excelente desenvolvedor",
+  },
+  {
+    name: "Eliane",
+    company: "Facens",
+    imgUrl: images.facens,
+    feedback: "Eduardo é um excelente desenvolvedor, trabalha em equipe",
+  },
+];
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img
+              src={testimonials[currentIndex].imgUrl}
+              alt={testimonials[currentIndex].name}
+            />
+            <div className="app__testimonial-content">
+              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <div>
+                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
+                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentIndex === 0
+                    ? testimonials.length - 1
+                    : currentIndex - 1
+                )
+              }
+            >
+              <HiChevronLeft />
+            </div>
+
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentIndex === testimonials.length - 1
+                    ? 0
+                    : currentIndex + 1
+                )
+              }
+            >
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="app__testimonial-brands app__flex">
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: "tween" }}
+            key={brand._id}
+          >
+            <img src={brand.imgUrl} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default Testimonials;
+export default AppWrap(
+  MotionWrap(Testimonials, "app__testimonial"),
+  "testimonial",
+  "app__primarybg"
+);
